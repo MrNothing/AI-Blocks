e0c3851bb3e3035be50088a7b7292f7331556b5b
Boris Musarais <boris1284@gmail.com>
2017-11-27 20:22:24 +0100

    added website

---
+++ docs/css/scrolling-nav.css
@@ -0,0 +1,19 @@
+/*!
+ * Start Bootstrap - Scrolling Nav (https://startbootstrap.com/template-overviews/scrolling-nav)
+ * Copyright 2013-2017 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-scrolling-nav/blob/master/LICENSE)
+ */
+
+header {
+  padding: 154px 0 100px;
+}
+
+@media (min-width: 992px) {
+  header {
+    padding: 156px 0 100px;
+  }
+}
+
+section {
+  padding: 150px 0;
+}
